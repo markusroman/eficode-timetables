c2fdf6b1f8207584ecdcfdc0f62f5aa9f2cd85da
Markus Röman <markus.roman@hotmail.fi>
2020-03-04 00:11:27 +0200

    Changed map to typescript.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { ThemeProvider } from '@material-ui/core/styles';
 // eslint-disable-next-line no-unused-vars
 import { mainTheme, darkTheme } from './AppStyles';
 import Timetable from './ts-components/Timetable';
-import BackgroundMap from './components/BackgroundMap';
+import BackgroundMap from './ts-components/BackgroundMap';
 // eslint-disable-next-line no-unused-vars
-import TestMap from './components/TestMap';
+import TestMap from './ts-components/TestMap';
 
 const App = () => {
   return (
